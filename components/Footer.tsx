00bb923c7ac5c3f2303a90ef4241ea300490761c
Diego <77334337+DiegoMartinezDeSanjuan@users.noreply.github.com>
2025-08-10 15:51:57 +0200

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="container" style={{paddingBottom:'1.25rem'}}>
+      <p className="muted">© {new Date().getFullYear()} Dos Plats · Lleida</p>
+    </footer>
+  );
+}
