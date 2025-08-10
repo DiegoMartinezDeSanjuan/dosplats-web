0968a0971d8c33f236e8b6d51787afe9cb10ebb5
Diego <77334337+DiegoMartinezDeSanjuan@users.noreply.github.com>
2025-08-10 16:51:28 +0200

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: process.env.NEXT_PUBLIC_DEFAULT_LOCALE || 'es',
     locales: ['es', 'ca', 'en'],
-    localeDetection: true,
+    localeDetection: false,
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
